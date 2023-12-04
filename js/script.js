0a522bcbb3f14fb7cf7f14be5d5d4e86003ccce8
madiinemm <uraimovamadina179@gmail.com>
2023-12-04 21:35:54 +0500

    masalalar ishlandi

---
+++ js/script.js
@@ -0,0 +1,110 @@
+ 1.// let n = +prompt("Sonni kiriting");
+// let sum = 0;
+// for (let i = 1;i <= n; i++ ) {
+//     if (i % 2 == 1) {
+//      sum += i;
+//     }
+// } console.log(sum);
+
+2.// let n = +prompt ("Sonni kiriting");
+// let sum = 1;
+// for(let i = n; i >= 1; i--) {
+//    if (i % 3 == 0)  
+//    sum *= i;      
+// } console.log(sum);
+
+3.// let a = +prompt("Birinchi sonni kiriting");
+// let b = +prompt("Ikkinchi sonni kiriting");
+// let sum = 0;
+// for (let i = a; i <= b; i++) {
+//   if (i % 3 == 0) {
+//     sum++;
+//   }
+// }
+// console.log(sum);
+
+
+4.// let a = +prompt("Birinchi sonni kiriting");
+// let b = +prompt("Ikkinchi sonni kiriting");
+// let sum = 0;
+// for (let i = a; i <= b; i++) {
+//   if (i % 5 == 0 && i % 7 == 0) {
+//     sum += i;
+//   }
+// }
+// console.log(sum);
+
+
+5.// let a = +prompt("Birinchi sonni kiriting");
+// let b = +prompt("Ikkinchi sonni kiriting");
+// let sum = 0;
+// for (let i = a; i <= b; i++) {
+//   if (i % 2 == 1) {
+//     sum +  = i ** 2;
+//   }
+// }
+// console.log(sum);
+
+6.
+// let a = +prompt("Birinchi sonni kiriting");
+// let b = +prompt("Ikkinchi sonni kiriting");
+// let sum = 1;
+// for (let i = a; i <= b; i++) {
+//   if (i % 2 == 0) {
+//     sum *= i ** 3;
+//   }
+// }
+// console.log(sum);
+
+7.
+// let n = +prompt("Sonni kiriting");
+// for (let i = 1; i <= n; i++) {
+//   if (n % i == 0) {
+//     console.log(i);
+//   }
+// }
+
+
+8.// let n = +prompt("Sonni kiriting");
+// count = 0
+// for (let i = 1; i <= n; i++) {
+//   if (n % i == 0) {
+//     count ++
+//   }
+// }
+// console.log(count)
+
+9.
+// let n = +prompt("Sonni kiriting");
+// let count = 0
+// let res;
+// for (let i = 1; i <= n; i++) {
+//   if (n % i == 0) {
+//     count++;
+//   }
+// }
+// if (count == 2) {
+//   res = "son tub";
+// } else {
+//   res = "tub emas";
+// }
+// console.log(res);
+
+10.
+
+// let a = +prompt("Birinchi sonni kiriting");
+// let b = +prompt("Ikkinchi sonni kiriting");
+// let sum = 0;
+// for (let i = a; i <= b; i++) {
+//   if (i % 3 == 0 && i % 7 != 0) {
+//     sum += i;
+//   }
+// }
+// console.log(sum);
+
+12.
+// let n = +prompt("Sonni kiriting");
+// let sum = 180 * (n - 2);
+// console.log(sum);
+
+
